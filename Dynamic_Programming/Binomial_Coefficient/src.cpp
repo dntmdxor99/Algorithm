e10e87c2f2a68943270e06a931ac30050c9d4f36
Woo <82219614+dntmdxor99@users.noreply.github.com>
2022-04-13 23:56:27 +0900

    Update src.cpp

--- Dynamic_Programming/Binomial_Coefficient/src.cpp
+++ Dynamic_Programming/Binomial_Coefficient/src.cpp
@@ -29,8 +29,73 @@ int bio(int arr[], int n, int k){
 }
 
 
-int main(){    
+int bin_recursion(int n, int k){
+    if ((k == 0) || (k == n)){
+        return 1;
+    }
+    else{
+        return bin_recursion(n-1, k-1) + bin_recursion(n-1, k);
+    }
+}
 
+
+int arr[100][100] = {0,};
+void bin_memoization(int i, int j){
+    if ((j == 0) || (j == i)){
+        arr[i][j] = 1;
+    }
+    else{
+        bin_memoization(i-1, j-1);
+        bin_memoization(i-1, j);
+        arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+    }
+}
+
+
+int bin_DP(int n, int k){
+    for (int i = 0; i <= n; i++){
+        for (int j = 0; j <= min(k, i); j++){
+            if ((j == 0) || (j == i)){
+                arr[i][j] = 1;
+            }
+            else{
+                arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+            }
+        }
+    }
+    return arr[n][k];
+}
+
+
+int optimal_bin(int n, int k, int MAX){
+    int B[100000] = {0,};       // 배열에 이항 계수를 저장함
+    k = min(k, n-k);        // k를 좀 더 작은 애로 집어 넣음
+
+    int temp, temp2;       // B[j-1]을 저장하는 원소
+
+    for (int i = 0; i <= n; i++){
+        for (int j = 0; j <= min(k, i); j++){
+            if ((j == 0) || (j == n)){     //j == 0 혹은 j == n과 같다면 1을 저장함
+                B[j] = 1;
+                temp = 1;
+            }
+            else{
+                // B[j] = B[j] + B[j-1];       // 이러면 B[j-1]은 이미 업데이트 된 상태이므로 기존의 B[j-1]을 저장한 무언가를 넣어줘야함
+                temp2 = B[j];       // 변환되기 전의 B[j]를 넣음
+                B[j] = (B[j] + temp) % MAX;
+                temp = temp2;        // 변환되기 전의 B[j]를 넣음, temp는 다음 반복문, 즉 j+1으로 가서 기존의 B[j]의 역할을 수행함
+            }
+        }
+        // for (int idx = 0; idx <= k; idx++){      // 과정을 출력함
+        //         cout << B[idx] << " ";
+        //     }
+        //     cout << endl;
+    }
+    return B[k];
+}
+
+
+int main(){    
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
@@ -39,13 +104,10 @@ int main(){
 
     cin >> n >> k >> MAX;       // 인자 받음
 
-    k = min(k, n-k);        // k를 좀 더 작은 애로 집어 넣음
-
-    int arr[100000] = {0,};
-    // int arr[k+1] = {0,};     // 이러면 동적으로 받아서 배열이 아닌 벡터로 되는듯 ..
+    
 
     if (k > 0)
-        cout << bio(arr, n ,k) << "\n";
+        cout << optimal_bin(n ,k, MAX) << "\n";
     else
         cout << 1 << "\n";
 
