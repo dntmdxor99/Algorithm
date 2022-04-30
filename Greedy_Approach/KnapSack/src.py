ffcd6ea29ebe103a2343bd606e180a4291059361
Woo <82219614+dntmdxor99@users.noreply.github.com>
2022-04-30 20:08:49 +0900

    Update src.py

--- Greedy_Approach/KnapSack/src.py
+++ Greedy_Approach/KnapSack/src.py
@@ -1,45 +1,43 @@
 def func(W, items):
-    bag = 0
-    sum = 0
-    temp = []
-    for item in items:
-        if W - bag >= item[0]:
-            bag += item[0]
-            sum += item[1]
-            temp.append([item[0], item[1]])
-        elif W - bag > 0:
-            temp.append([0, 0])
+    temp = []       # 가방에 넣은 무게와 이익을 저장하는 리스트
+    for item in items:      # 하나씩 가져옴
+        if W - item[0] >= 0:
+            # 만약 통쨰로 넣어도 문제가 없다면 W에서 item[1]을 뺌
+            W -= item[0]
+            temp.append([item[0], item[1]])     # 리스트에 넣음
+        elif W > 0:
+            # 통째로 넣기에는 무리가 있을 때
+            temp.append([0, 0])     # 일단 0,0을 넣음
             for _ in range(item[0]):
-                if bag + 1 <= W:
-                    bag += 1
-                    sum += item[2]
+                # 1의 무게와 무게당 이익을 추가함
+                if W > 0:
+                    # 아직 무게가 남아있다면 1을 뺴고 temp에 추가함
+                    W -= 1
                     temp[-1][0] += 1
                     temp[-1][1] += item[2]
-                elif bag + 1 > W:
-                    return temp, sum
-    return temp[:-1], sum
+                elif W <= 0:
+                    # 만약 무게가 꽉 찼다면 그대로 반환함
+                    return temp
+        else:
+            # 만약 W <= 0일 때
+            return temp
+    # 무게를 다 못 채울 때
+    return temp
 
 if __name__ == "__main__":
     n = int(input())        # 아이템의 개수
-    weight = [0]
-    weight.extend(list(map(int, input().split())))      # 아이템의 무게를 입력 받음
-    profit = [0]
-    profit.extend(list(map(int, input().split())))       # 아이템의 이익을 입력 받음
-    ppw = [0]       # profit per weight
-    ppw.extend([y // x for y, x in zip(profit[1:], weight[1:])])
+    weight = list(map(int, input().split()))      # 아이템의 무게를 입력 받음
+    profit = list(map(int, input().split()))       # 아이템의 이익을 입력 받음
 
-    item = [(x, y, z) for x, y, z in zip(weight, profit, ppw)]      # 무게, 이익, 무게당 이익
-    item.sort(key = lambda x : x[2], reverse=True)
+    item = [(x, y, y // x) for x, y in zip(weight, profit)]      # 무게, 이익, 무게당 이익
+    item.sort(key = lambda x : x[2], reverse=True)      # 무게당 이익을 기준으로 정렬함
 
     T = int(input())        # 도둑의 배낭 개수
 
     items = item
     for _ in range(T):
         W = int(input())        # 도둑의 배낭 무게
-        temp, sum_profit = func(W, items)
-        print(sum_profit)
+        temp = func(W, items)
+        print(sum(map(lambda x : x[1], temp)))
         for t in temp:
             print(*t, sep=' ')
-
-
-
