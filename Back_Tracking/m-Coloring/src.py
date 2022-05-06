96a09990cb75afee2d50d88ee9f06392b27608ac
Woo <82219614+dntmdxor99@users.noreply.github.com>
2022-05-06 17:28:05 +0900

    Create src.py

---
+++ Back_Tracking/m-Coloring/src.py
@@ -0,0 +1,42 @@
+def promising(i: int) -> bool:
+    j = 1
+    while j < i:
+        if W[i][j] and vcolor[i] == vcolor[j]:
+            return False
+        j += 1
+    return True
+
+
+def m_coloring(i: int, m: int) -> bool:
+    global flag
+    global cnt
+    if promising(i):
+        if i == ver_num:
+            flag = True
+            cnt += 1
+        else:
+            for color in range(1, m + 1):
+                vcolor[i + 1] = color
+                m_coloring(i + 1, m)
+
+
+if __name__ == "__main__":
+    ver_num, edge_num = map(int, input().split())
+    W = [[0] * (ver_num + 1) for _ in range(ver_num + 1)]
+    for _ in range(edge_num):
+        i, j = map(int, input().split())
+        W[i][j] = 1
+        W[j][i] = 1
+
+    # print(*W, sep='\n')
+
+    vcolor = [0] * (ver_num + 1)
+
+    for m in range(1, 5):
+        flag = False
+        cnt = 0
+        m_coloring(1, m)
+        if flag:
+            print(m + 1)
+            print(cnt * (m + 1))
+            break
