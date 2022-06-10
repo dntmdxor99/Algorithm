e4ee89172b4181d8abd35804d53a8205e96bd8c1
Woo <82219614+dntmdxor99@users.noreply.github.com>
2022-06-10 13:35:59 +0900

    Update src.py

--- Math/BOJ 1978/src.py
+++ Math/BOJ 1978/src.py
@@ -1,12 +1,14 @@
-_ = int(input())
-N = list(map(int, input().split()))
+_ = int(input())        # 필요 없음
+N = list(map(int, input().split()))     # 숫자들을 입력 받음
 
-arr = [True] * (max(N) + 1)      # 0은 소수라는 뜻
-arr[0] = arr[1] = False      # 1은 소수가 아님
+arr = [True] * (max(N) + 1)      # True은 소수라는 뜻
+arr[0] = arr[1] = False      # False은 소수가 아님
 
 for i in range(2, max(N) + 1):
+    # 2부터 최고 숫자까지 에라토스테네스의 체를 계산함
     if arr[i]:
         for j in range(i * 2, max(N) + 1, i):
+            # i의 배수들을 모두 False 처리함
             arr[j] = False
     
 
