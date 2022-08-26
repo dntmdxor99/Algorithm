918549cb86222fdf68f9b30b3644fde36c32a663
dntmdxor99 <dntmdxor99@naver.com>
2022-08-26 15:20:12 +0900

    .

---
+++ BOJ_1697_숨바꼭질.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+
+def bfs(que : deque, maps : list):
+    while que:
+        x, cnt = que.popleft()
+        if x == k:
+            return cnt
+        for nx in [x - 1, x + 1, 2 * x]:
+            if 0 <= nx <= 100000 and maps[nx] == 0:
+                maps[nx] = cnt + 1
+                que.append((nx, cnt + 1))
+                
+        
+n, k = map(int, input().split())
+
+maps = [0] * 100001
+maps[n] = 1
+que = deque()
+que.append((n, 0))
+cnt = bfs(que, maps)
+print(cnt)
+    
+    
